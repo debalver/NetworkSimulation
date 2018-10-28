b226a6ea773f342821adda550f6ff31804cd742a
debalver <quentin.devaud@epfl.ch>
2018-10-28 21:15:49 +0100

    First version of network.cpp

---
+++ src/network.cpp
@@ -0,0 +1,39 @@
+#include "network.h"
+#include <iostream> 
+
+void Network::resize(const size_t&)
+{
+	
+}
+bool Network::add_link(const size_t&, const size_t&)
+{
+	
+}
+size_t Network::random_connect(const double&)
+{
+	
+}
+size_t Network::set_values(const std::vector<double>&)
+{
+	
+}
+size_t Network::size() const
+{
+	
+}
+size_t Network::degree(const size_t &_n) const
+{
+	
+}
+double Network::value(const size_t &_n) const
+{
+	
+}
+std::vector<double> Network::sorted_values() const
+{
+	
+}
+std::vector<size_t> Network::neighbors(const size_t&) const
+{
+	
+}
